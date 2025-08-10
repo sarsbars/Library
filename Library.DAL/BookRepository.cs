429eb33b7f0a149381fcc83346986c0a934014e0
Connor Hall <connrhall204@gmail.com>
2025-08-09 22:22:50 -0500

    Initial File Setup

---
+++ Library.DAL/BookRepository.cs
@@ -0,0 +1,15 @@
+﻿using Library.Models;
+
+namespace Library.DAL {
+    public class BookRepository {
+        private readonly LibraryDBContext _context;
+
+        public BookRepository (LibraryDBContext context) {
+            _context = context;
+        }
+
+        public List<Book> GetBooks() {
+            return _context.Books.ToList();
+        }
+    }
+}
---
+++ Library/Controllers/BookController.cs
@@ -0,0 +1,14 @@
+﻿using Library.BLL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers {
+    public class BookController : Controller {
+        private readonly BookService _bookService;
+        public BookController (BookService bookService) {
+            _bookService = bookService;
+        }
+        public IActionResult Index () {
+            return View();
+        }
+    }
+}

dfc7f9eca06a69dafcbc6be2c2f334f5186bf55c
Ayomide Boye <boyeayomide@gmail.com>
2025-08-11 06:37:02 -0500

    created some of my files and added my own appsetting file

---
+++ Library/Controllers/UserController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers {
+    public class UserController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}

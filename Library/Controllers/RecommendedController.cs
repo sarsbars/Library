0f5a3fc455023efcd24ce77f15789585bbc1705d
Connor Hall <connrhall204@gmail.com>
2025-08-13 16:37:25 -0500

    Added controller

---
+++ Library/Controllers/RecommendedController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers {
+    public class RecommendedController : Controller {
+        public IActionResult Recommended () {
+            return View();
+        }
+    }
+}

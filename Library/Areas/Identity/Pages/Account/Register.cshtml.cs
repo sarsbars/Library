13f05fc4a554e2691f23e1eaf50d1ab829ac4a02
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-08-13 09:36:39 -0500

    updated _layout and added line to register.cshtml.cs

--- Library/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Library/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -120,6 +120,9 @@ namespace Library.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+
+                    await _userManager.AddToRoleAsync(user, "User");
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);

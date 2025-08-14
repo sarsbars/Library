a3a54abcfdbc8a53de0a94f5226ee9726a93b4b9
Ashedzi Solomon <solomonashe84@gmail.com>
2025-08-13 22:49:34 -0500

    Create Dashboardview model

---
+++ Library.Models/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models {
+    public class DashboardViewModel {
+        public int TotalBooks { get; set; }
+        public int TotalUsers {get; set;}
+        public int TotalLoans { get; set; }
+        public int OverDueBooksCount { get; set; }
+        public ICollection<Book> MostRecentlyAddedBooks { get; set; }
+        public ICollection<Book> AvailableBooks { get; set; }
+        public ICollection<User> TopBorrowers { get; set; }
+    }
+}

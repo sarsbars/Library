18162d6b58c61e007cea48dc861aca2be91f6ef8
Ashedzi Solomon <solomonashe84@gmail.com>
2025-08-13 22:56:01 -0500

    Create Dashboard controller

--- Library/Controllers/DashboardController.cs
+++ Library/Controllers/DashboardController.cs
@@ -1,7 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Library.BLL;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Controllers {
     public class DashboardController : Controller {
+        private readonly UserService _userService;
+        private readonly LoanService _loanService;
+        private readonly LocationService _locationService;
+        private readonly BookService _bookService;
+
+        public DashboardController(
+            UserService userService,
+            LoanService loanService,
+            LocationService locationService,
+            BookService bookService) {
+            _userService = userService;
+            _loanService = loanService;
+            _locationService = locationService;
+            _bookService = bookService;
+        }
         public IActionResult Index() {
             return View();
         }

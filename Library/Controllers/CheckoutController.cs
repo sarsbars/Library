8a185de8366ff94edcce3b1438e600213183646e
Ayomide Boye <boyeayomide@gmail.com>
2025-08-14 20:41:50 -0500

    checkoutcontroller done

--- Library/Controllers/CheckoutController.cs
+++ Library/Controllers/CheckoutController.cs
@@ -1,27 +1,40 @@
-﻿using Library.BLL;
+﻿using Library.DAL;
+using Library.Models;
 using Library.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Library.Controllers {
     public class CheckoutController : Controller {
-        private readonly HoldService _holdService;
-        private readonly LoanService _loanService;
-        private readonly BookService _bookService;
+        private readonly LibraryDBContext _context;
 
-        public CheckoutController(
-            HoldService holdService,
-            LoanService loanService,
-            BookService bookService) {
-            _holdService = holdService;
-            _loanService = loanService;
-            _bookService = bookService;
+        public CheckoutController(LibraryDBContext context) {
+            _context = context;
         }
 
         public IActionResult Index() {
-            var model = new CheckoutViewModel {
-                HoldsWaitingPickup = _holdService.GetHoldsWaitingForPickup(),
-                CurrentLoans = _loanService.GetCurrentLoans(),
-                RecentBooks = _bookService.GetMostRecentBooks(5)
+            var model = new CheckoutViewModel {               
+                Holds = _context.Loans
+                    .Include(l => l.Book)
+                    .Include(l => l.User)
+                    .Include(l => l.Location)
+                    .Where(l => l.LoanStatus == LoanStatusType.OnHold)
+                    .OrderByDescending(l => l.DateBorrowed)
+                    .ToList(),
+             
+                CurrentLoans = _context.Loans
+                    .Include(l => l.Book)
+                    .Include(l => l.User)
+                    .Include(l => l.Location)
+                    .Where(l => l.LoanStatus == LoanStatusType.TakenOut)
+                    .OrderByDescending(l => l.DateBorrowed)
+                    .ToList(),
+
+                RecentBooks = _context.Books
+                    .OrderByDescending(b => b.BookID) 
+                    .Take(5)
+                    .ToList()
             };
 
             return View(model);

d100139017d8ee964711c478a90be750a4cbf0bb
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-08-15 09:32:26 -0500

    debugged location name issue

---
+++ Library.DAL/Migrations/20250815143137_location-name.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class locationname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationID",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomName",
+                table: "Locations",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "LocationID",
+                keyValue: 1,
+                column: "CustomName",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "LocationID",
+                keyValue: 2,
+                column: "CustomName",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "LocationID",
+                keyValue: 3,
+                column: "CustomName",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "LocationID",
+                keyValue: 4,
+                column: "CustomName",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "LocationID",
+                keyValue: 5,
+                column: "CustomName",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomName",
+                table: "Locations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationID",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
